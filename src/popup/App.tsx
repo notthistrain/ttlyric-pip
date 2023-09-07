c241c918044a1d0174fa477025f442677f382095
jack <940457524@qq.com>
2023-09-07 23:01:28 +0800

    feat: 验证实时插入脚本可行性

--- src/popup/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function () {
-  return (
-    <div m-4>
-      <span text-green>爱</span>
-      <span s-red>江西</span>
-      <span hover:text-blue>婷婷</span>
-    </div>
-  )
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,70 +1,16 @@
-import { PluginOption, defineConfig } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import unocss from "unocss/vite"
 import { resolve } from "path"
-import { createServer } from "http"
-
-/**
- * @description 代码重新构建后通知拓展后台脚本重新加载
- * @returns vite插件配置项
- */
-const reload = (): PluginOption => {
-  /**
-   * @description http连接数有限，建立新tcp连接后把其他连接断开
-   */
-  let connLock
-  let msgLock = false
-  let server = createServer((request, response) => {
-    if (request.url === "/sse") {
-      connLock = Date.now()
-      const resSignal = connLock
-      console.log(`client request  ${resSignal}`)
-      response.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        Connection: "keep-alive",
-        "Access-Control-Allow-Origin": "*",
-      })
-
-      const timer = setInterval(() => {
-        if (resSignal !== connLock) {
-          console.log(`client close  ${resSignal}`)
-          clearInterval(timer)
-          response.end()
-          return
-        }
-        if (!msgLock) {
-          msgLock = true
-          response.write(`data: reload\n\n`)
-        }
-      }, 1000)
-
-      return
-    }
-  })
-  server.listen(3347, "127.0.0.1", () => {
-    console.log("\nsse server listen...\n")
-  })
-  return {
-    name: "crx-reload-plugin",
-    buildStart(options) {
-      this.addWatchFile(resolve(__dirname, "./public"))
-    },
-    closeBundle() {
-      msgLock = false
-    },
-    closeWatcher() {
-      console.log("\nsse server close...\n")
-      server.close()
-      server = null
-    },
-  }
-}
+import { CRXReloadPlugin as reload } from "./vite_plugin/CRXReloadPlugin"
 
 export default defineConfig({
   plugins: [unocss(), react(), reload()],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "@": resolve("src"),
+    },
   },
   publicDir: "public",
   build: {
@@ -74,7 +20,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, "./popup.html"),
-        background: resolve(__dirname, "./src/background"),
+        background: resolve(__dirname, "./src/main/background"),
+        netease: resolve(__dirname, "./src/main/insert/netease"),
       },
       output: {
         dir: "pkg",
